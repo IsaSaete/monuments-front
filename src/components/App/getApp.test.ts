97d4b4a333cdda9d2b7aa2ca1041260460f737e0
Isabel Sáenz <isasaete@gmail.comn>
2025-04-03 12:09:14 +0200

    Modify test getApp & getHeader

--- src/components/App/getApp.test.ts
+++ src/components/App/getApp.test.ts
@@ -4,17 +4,16 @@ describe("Given the App component", () => {
   describe("When it renders", () => {
     test("Then it should show 'Monuments of the world' inside a heading", () => {
       const screen = document.createElement("div");
-      const expectedTitle = "Monuments of the World";
+      const expectedTitle = "Monuments of the world";
 
       const App = getApp();
 
       screen.appendChild(App);
 
-      const header = screen.querySelector("header");
-      const titleHeader = header?.querySelector("h1");
+      const titleApp = screen.querySelector("h1");
 
-      expect(header).not.toBeNull();
-      expect(titleHeader?.textContent).toBe(expectedTitle);
+      expect(titleApp).not.toBeNull();
+      expect(titleApp?.textContent).toBe(expectedTitle);
     });
   });
 });
